fcd8343f09819b5d704d78b90f83952fa59bfbfe
lineageispig <lineageispig@gmail.com>
2017-09-21 18:25:36 +0800

    commit1

---
+++ js/map.js
@@ -0,0 +1,283 @@
+
+// Creat a map function
+function initMap() {
+  var bounds = new google.maps.LatLngBounds;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13
+});
+
+  var input = document.getElementById('pac-input');
+  var inp = document.getElementById('query!-input')
+}
+
+
+
+// Create a function which include Google map api Autocomplete, DirectionService and DistanceMatrix. 
+function initAutocomplete() {
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 22.372081, lng: 114.107877},
+    zoom: 7,
+    mapTypeId: 'roadmap'
+});
+        
+  var infowindow = new google.maps.InfoWindow(); 
+  var marker = new google.maps.Marker({
+  map: map
+});
+         
+marker.addListener('click', function() {
+            
+            infowindow.close();
+            var place = autocomplete.getPlace();
+            if (!place.place_id) {
+return; 
+}
+})
+
+    
+var input = document.getElementById('origin-input');
+var input2 = document.getElementById('destination-input');
+    // this is the test code.
+      var address = document.getElementById('origin-input').value;
+     var address2 =  document.getElementById('destination-input').value;   
+  
+  var autocomplete =  new google.maps.places.Autocomplete(
+  input, {placeIdOnly: true}); 
+  var autocomplete2 =  new google.maps.places.Autocomplete(
+  input2, {placeIdOnly: true}); 
+
+  autocomplete.bindTo('bounds', map);
+  autocomplete2.bindTo('bounds', map);
+          
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input2);
+
+  var infowindow = new google.maps.InfoWindow(); 
+  var geocoder = new google.maps.Geocoder();
+  var marker = new google.maps.Marker({
+  map: map
+}); 
+
+  marker.addListener('click', function() {
+  infowindow.open(map, marker);
+});
+    
+    autocomplete.addListener('place_change', function() {
+  infowindow.close();
+  var place = autocomplete.getPlace();
+      if (!place.place_id) {
+          return ("there is no place id"); 
+}
+  geocoder.geocode({'placeId': place.place_id}, function(results, status){
+      if (status !=='OK') {
+          window.alert('Geocoder failed due to: ' + status );
+          return;
+  }
+})
+})  
+    
+        
+  directionsDisplay.setMap(map); 
+  directionsDisplay.setPanel(document.getElementById('right-panel')); 
+      
+  var onChangeHandler = function() {
+   calculateAndDisplayRoute(directionsService, directionsDisplay)
+}; 
+          
+  
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('origin-input').value,
+    destination: document.getElementById('destination-input').value, 
+    travelMode: 'DRIVING'
+}, function(response, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+          
+          directionsDisplay.setDirections(response);
+          
+
+     var geocoder = new google.maps.Geocoder();
+             
+
+     var address = document.getElementById('origin-input').value;
+     var address2 =  document.getElementById('destination-input').value;
+    
+    getLocation(address);
+  
+
+    }
+  });
+};
+
+           
+var getLocation =  function(address) {
+  var geocoder = new google.maps.Geocoder();
+ 
+  geocoder.geocode( { 'address': address}, function(results, status) {
+
+     
+  if (status == google.maps.GeocoderStatus.OK) {
+      
+       var array =[]; 
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+
+      var latlng = [latitude,  longitude];
+   
+      array.push(latlng);
+
+     console.log(latlng);
+     console.log(array[0]) 
+ 
+      
+      document.getElementById("demo").value = latitude;  
+      document.getElementById("demo").textContent = latitude
+      
+      
+    document.getElementById("demo2").value = longitude;  
+    document.getElementById("demo2").textContent = longitude
+//}
+      return latlng
+  
+      }
+      return latlng
+  }); 
+
+}
+
+
+
+
+ 
+document.getElementById("myBtn").addEventListener("click", function(){
+    
+
+  onChangeHandler();  
+
+    console.log("Hello World"); 
+
+});
+      
+
+var getLocation2 =  function(address2) {
+  var geocoder = new google.maps.Geocoder();
+ 
+  geocoder.geocode( { 'address': address}, function(results, status) {
+
+  if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+      var latlng = [latitude,  longitude];
+      console.log(latitude, longitude);
+
+        document.getElementById("button2").textContent = latitude
+    var x = document.getElementById("button2").textContent;
+    document.getElementById("demo2").innerHTML = longitude;  
+   
+
+//}
+      
+      return latlng
+
+      } 
+
+      return latlng
+  }); 
+
+}
+
+// I want the third format that present the distance
+// this is the location of the singal point 
+// all is well 
+// 
+var getLocation2 =  function(address2) {
+  var geocoder = new google.maps.Geocoder();
+ 
+  geocoder.geocode( { 'address': address}, function(results, status) {
+
+  if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+      var latlng = [latitude,  longitude];
+      console.log(latitude, longitude);
+
+        document.getElementById("button2").textContent = latitude
+    var x = document.getElementById("button2").textContent;
+    document.getElementById("demo2").innerHTML = longitude;  
+   
+
+//}
+      
+      return latlng
+
+      } 
+
+      return latlng
+  }); 
+
+}
+
+
+
+// I want the forth format that present the time
+
+  getLocation2(address2);
+    
+};  
+ 
+function mFunction() {
+  var address1 = document.getElementById('origin-input').value;
+  var address2 = document.getElementById('destination-input').value;
+  window.open("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + address1 + "&destinations=" + address2 + '&key=AIzaSyDH3Y5os8GvSV1IgwOXYvS50PeZbsBAFN8' ); 
+}
+
+        
+function SFunction() {
+  var address1 = document.getElementById('origin-input').value;
+  var address2 = document.getElementById('destination-input').value;
+  window.open("https://maps.googleapis.com/maps/api/geocode/json?address=" + address1 + '&key=AIzaSyDH3Y5os8GvSV1IgwOXYvS50PeZbsBAFN8' );
+}
+        
+function EFunction() {
+  var address1 = document.getElementById('origin-input').value;
+  var address2 = document.getElementById('destination-input').value;
+  window.open("https://maps.googleapis.com/maps/api/geocode/json?address="  + address2 + '&key=AIzaSyDH3Y5os8GvSV1IgwOXYvS50PeZbsBAFN8'
+)};
+
+
+
+      $(document).ready(function(){
+        var user,pass;
+        $("#myBtn").click(function(){
+          Lat=$("#origin-input").val();
+          Lng=$("#destination-input").val();
+          $.post("http://localhost:8080",{user: Lat,password: Lng}, function(data){
+            if(data==='done')
+              { 
+ 
+                alert("login success");
+              }
+          });
+        });
+      });
+
+      $(document).ready(function(){
+        var lat,lng;
+        $("#myBtn").click(function(){
+          lat=$("#demo").val();
+          lng=$("#demo2").val();
+          $.post("http://localhost:8080",{lat: lat,lng: lng}, function(data){
+            if(data==='done')
+              { 
+
+                alert("login success");
+              }
+          });
+        });
+      });
+
+// I am sick of that kind of shit 
---
+++ server.js
@@ -0,0 +1,75 @@
+'use strict'
+
+var request = require("request");
+var nodeStatusCodes = require('node-status-codes');
+var express = require('express');
+var bodyParser = require('body-parser'); 
+var morgan          = require('morgan');
+var app = express(); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('short'));
+app.use(express.static(__dirname)); 
+
+app.get('/', function(req, res) {
+
+    res.sendfile('/Users/mac/Desktop/myProject/myApi/myproduct.html');
+})
+
+app.post('/', function(req, res){
+
+  console.log('access success!') 
+
+  var user_name=req.body.user;
+  var password=req.body.password;
+  var lat = req.body.lat;
+  var lng = req.body.lng; 
+  var url =  "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + user_name + "&destinations=" + password  + '&key=AIzaSyDH3Y5os8GvSV1IgwOXYvS50PeZbsBAFN8',
+    url2 = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_name + '&key=AIzaSyDH3Y5os8GvSV1IgwOXYvS50PeZbsBAFN8',
+    url3 = "https://maps.googleapis.com/maps/api/geocode/json?address="  + password + '&key=AIzaSyDH3Y5os8GvSV1IgwOXYvS50PeZbsBAFN8';
+
+   console.log(" The starting point is "+user_name+" and the ending point is "+password);
+
+     console.log(" latitude= "+lat+" and longitude "+lng);
+
+    request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+        console.log('"status":' +body.status)
+    }
+})
+   request({ 
+    url: url2,
+    json: true
+}, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+    console.log('"START":' + '[' + body.results[0].geometry.location.lat + ',' + body.results[0].geometry.location.lng  +']')
+    }
+})
+    request({ 
+    url: url3,
+    json: true
+}, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+    console.log( '"DROPOFF":' + '[' + body.results[0].geometry.location.lat + ',' + body.results[0].geometry.location.lng + ']')
+    }
+})
+    request({ 
+    url: url,
+    json: true
+}, function (error, response, body) {
+    if (!error && response.statusCode === 200) {   
+        console.log('"total_distance":' +body.rows[0].elements[0].distance.value) 
+        console.log('"total_time":' +body.rows[0].elements[0].duration.value)
+    }
+})
+  res.end("yes");
+});
+
+app.listen(8080, function(){
+    
+    console.log('port success on 8080.')
+})
+
+
